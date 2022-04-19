e5224140c54fbd6f21ba5e807e58b8044c8e1cda
eddycyh <eddycyh@gmail.com>
2022-04-19 20:59:39 +0800

    - Added Qt systray widgets.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+﻿// qt-nasa-systray.cpp : Defines the entry point for the application.
+//
+
+#include <QApplication>
+#include <QMessageBox>
+
+#include "window.h"
+
+int main(int argc, char* argv[])
+{
+    Q_INIT_RESOURCE(systray);
+
+    QApplication app(argc, argv);
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(0, QObject::tr("Systray"),
+            QObject::tr("Error, Qt object not found "
+                "on this system."));
+        return 1;
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
+
+    Window window;
+    window.show();
+    return app.exec();
+}
